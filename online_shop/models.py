fbe12c8050f8dcc200d92b464a3b1bfef3223673
JohanJo11 <johanpambuku5@gmail.com>
2026-01-31 18:04:22 +0100

    Improve UI, add product images and printable invoice

--- online_shop/models.py
+++ online_shop/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
-        return f"{self.name} - {self.price} dollars"
+        return self.name
